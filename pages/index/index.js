38905dd1ee1c8edf47e34b36dc2c4940bd275149
jingjianqian <18697998680@163.com>
2022-12-03 20:24:13 +0800

    首页布局

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,16 @@
 // index.js
-Page({})
+Page({
+  data:{
+    list:[
+        {
+            pagePath:"/pages/index/index",
+            text:"首页",
+            iconPath:""
+        },
+        {
+          pagePath:"/pages/my/index",
+          text:"我的"
+      }
+    ]
+  }
+})
