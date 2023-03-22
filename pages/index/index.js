51139e19f52b42b52f50677d69be25b073e62a40
joker <18697998680@163.com>
2023-03-22 10:40:55 +0800

    update tabbar

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,5 +3,15 @@ var app = getApp();
 Page({
   data:{
     tabbar:{}
+  },
+  pageLifetimes: {
+    show() {
+      if (typeof this.getTabBar === 'function' &&
+        this.getTabBar()) {
+        this.getTabBar().setData({
+          selected: 0
+        })
+      }
+    }
   }
 })
