dc6d28d9244451ad638f7fa787515e6d2f93c613
joker <18697998680@163.com>
2023-03-23 21:18:19 +0800

    修复tab错误

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -5,25 +5,25 @@ Component({
     selectedColor: "#3cc51f",
     list: [
       {
-      pagePath: "/pages/index/index.wxml",
+      pagePath: "/pages/index/index",
       iconPath: "static/tabbar/index.png",
       selectedIconPath: "static/tabbar/index-hover.png",
       text: "首页1"
     }, 
     {
-      pagePath: "/pages/spider/spider.wxml",
+      pagePath: "/pages/spider/spider",
       iconPath: "static/tabbar/wode.png",
       selectedIconPath: "static/tabbar/wode-hover.png",
       text: "爬虫2"
     },
     {
-      pagePath: "/pages/blog/blog.wxml",
+      pagePath: "/pages/blog/blog",
       iconPath: "static/tabbar/wode.png",
       selectedIconPath: "static/tabbar/wode-hover.png",
       text: "博客3"
     },
     {
-      pagePath: "/pages/my/my.wxml",
+      pagePath: "/pages/my/my",
       iconPath: "static/tabbar/wode.png",
       selectedIconPath: "static/tabbar/wode-hover.png",
       text: "我的4"
@@ -34,12 +34,29 @@ Component({
   },
   methods: {
     switchTab(e) {
+      console.log(e)
       const data = e.currentTarget.dataset
       const url = data.path
-      wx.switchTab({url})
+      console.log(url)
+      wx.switchTab(
+        {
+          url:url,
+          success: res => {
+            console.log(res)
+          },
+          fail:res => {
+            console.log(res)
+          },
+          complete: res => {
+            console.log("12312321321312312")
+           console.log(this.getPageId)
+          },
+        }
+      )
       this.setData({
         selected: data.index
       })
+      console.log(data.index)
     }
   }
 })
