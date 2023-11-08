781f68c65fa8d5e6386b32073cb786f600a49c86
jingjianqian <18697998680@163.com>
2023-11-08 20:09:52 +0800

    *
    增加连接wifi连接功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -35,11 +35,65 @@ Page({
     wx.requestSubscribeMessage({
       tmplIds: [tempid],
       success(res){
-        console.log("success!")
+        console.log("消息订阅,success!")
         console.log(res)
       },
       fail(res){
         console.log("fail!")
+        console.log(res)
+      }
+    })
+  },
+  connWifi(event){
+    wx.startWifi({
+      success (res) {
+        console.log(res)
+        console.log('初始化wifi模块成功')
+        if(res.errMsg == 'startWifi:ok'){
+          wx.getWifiList({
+            success: function (res) {
+              console.log('获取wifi列表成功')
+              console.log(res)
+              wx.onGetWifiList(function(res) {
+                console.log('解析wifi列表')
+                console.log(res)
+                wifiList = []
+                let tmpList = []
+                if (res && res.wifiList) {
+                  res.wifiList.map(item => {
+                    if (tmpList.indexOf(item.SSID) == -1) {
+                      tmpList.push(item.SSID)
+                      wifiList.push(item)
+                    }
+                  })
+                console.log(wifiList)
+                console.log('我要连接WIFI了')
+                wx.connectWifi({
+                  SSID: 'Primeton',
+                  password: 'Primeton@Gxlq2023',
+                  success (res) {
+                    console.log('wifi连接成功呢')
+                    console.log(res.errMsg)
+                  },
+                  fail(res){
+                    console.log('oh,shit,连不上呢')
+                    console.log(res.errMsg)
+                  }
+                })
+                }
+              });
+            },
+            fail: function (res) {
+              console.log(res);
+            }
+          })
+        }
+      }
+    })
+  }
+  ,showWifiList(event){
+    wx.getWifiList({
+      success(res){
         console.log(res)
       }
     })
